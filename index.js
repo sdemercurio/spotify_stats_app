12d7e79deb8af4bf0efe5003ba017537349997f6
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-12-08 13:14:30 -0500

    development environment set with CLIENT_ID CLIENT_SECRET REDIRECT_URI and .env

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const port = 8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 //Route to home page
 app.get('/', (req, res) => {
@@ -18,7 +23,7 @@ app.get('/awesome-generator', (req, res) => {
     'not'} awesome`);
 })
 
-const port = 8000;
+
 app.listen(port, () => {
     console.log(`Express app listening at http://localhost:${port}`);
 });
