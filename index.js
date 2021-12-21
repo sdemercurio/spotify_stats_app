0c587597676d26b8dff4cb67c7880dfa221c70ce
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-12-21 14:22:44 -0500

    accessing access token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const querystring = require('querystring');
 const app = express();
+const axios = require('axios');
 const port = 8888;
 
 const CLIENT_ID = process.env.CLIENT_ID;
@@ -34,29 +35,66 @@ const generateRandomString = length => {
     return text;
 };
 
-const stateKey = 'spotify_auth_state';
+const statekey = 'spotify_auth_state';
 
 app.get('/login', (req, res) => {
     const state = generateRandomString(16);
-    res.cookie(stateKey, state);
+    res.cookie(statekey, state);
 
     const scope = 'user-read-private user-read-email';
 
     const queryParams = querystring.stringify({
         client_id: CLIENT_ID,
         response_type: 'code',
-        redirect_url: REDIRECT_URI,
+        redirect_uri: REDIRECT_URI,
         state: state,
-        scope: scope,
+        scope: scope
     })
     // hit login route authorize endpoint and redirect to Spotify
     res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
-
 app.get('/callback', (req, res) => {
-    res.send('callback');
+    const code = req.query.code || null;
+
+    axios({
+        method: 'post',
+        url:'https://accounts.spotify.com/api/token',
+        data: querystring.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: REDIRECT_URI
+        }),
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+        },
+    })
+    .then(response => {
+        if (response.status === 200) {
+
+            const { access_token, token_type } = response.data;
+
+            axios.get('https://api.spotify.com/v1/me', {
+                headers: {
+                    Authorization: `${token_type} ${access_token}`
+                }
+            })
+            .then(response => {
+                res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+            })
+            .catch(error => {
+                res.send(error);
+            });
+        } else {
+            res.send(response);
+        }
+    })
+    .catch(error => {
+        res.send(error);
+    })
 })
+
 
 
 app.listen(port, () => {
